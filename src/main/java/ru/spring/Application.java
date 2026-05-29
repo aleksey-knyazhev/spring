3ae7ad5d26404cb88e9337891bcb7649c837608c
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2026-05-29 22:12:03 +0300

    Initial commit

--- src/main/java/ru/spring/Application.java
+++ src/main/java/ru/spring/Application.java
@@ -2,9 +2,11 @@ package ru.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class Application {
+    @Transactional
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
